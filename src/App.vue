a3b8b2a79b8753f36d95af68aa30e9698aa0ee20
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2024-03-02 19:26:17 +0300

    Initial commit, create App vs page with sorting by tags

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<BlogDetails />
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BlogDetails from './pages/BlogDetails.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BlogDetails
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/pages/BlogDetails.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <Header />
+    <BlogDetailsBanner />
+    <BlogDetailsArticle />
+    <Footer />
+  </template>
+  
+  <script>
+  import Header from '../components/Header.vue'
+  import Footer from '../components/Footer.vue'
+  import BlogDetailsBanner from '../components/BlogDetailsBanner.vue'
+  import BlogDetailsArticle from '../components/BlogDetailsArticle.vue'
+  
+  export default {
+    name: 'BlogDetails',
+    components: {
+      Header, 
+      Footer, 
+      BlogDetailsBanner,
+      BlogDetailsArticle,
+    }
+  }
+  </script>
+  
+  <style lang="scss">
+  
+  </style>
